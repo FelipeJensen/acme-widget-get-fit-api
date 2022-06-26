f9e0d9c3941cf111b55a7404917f3f1e1dd58fae
Felipe Jensen <felipe.jensen@venuiti.com>
2022-06-26 02:04:16 -0400

    Added enum descriptions to swagger

--- AcmeWidget.GetFit.Api/Program.cs
+++ AcmeWidget.GetFit.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using AcmeWidget.GetFit.Api;
 using AcmeWidget.GetFit.Api.ServiceCollectionExtensions;
 using AcmeWidget.GetFit.Application;
@@ -8,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwagger();
